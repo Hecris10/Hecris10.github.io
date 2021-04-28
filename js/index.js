93b5df98ab90896eb395ba8395a15da98899d682
Hecris10 <38855938+Hecris10@users.noreply.github.com>
2021-04-27 23:28:31 -0300

    Updating

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.querySelector('#lastupdate').textContent = document.lastModified;
+var year = new Date().getFullYear();
+window.alert(year);
+document.querySelector('#year').textContent = year;
